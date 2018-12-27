86470b9a9d4ad07d8497996875cdf584e2a5f418
thegreatsantini <lss13@my.fsu.edu>
2018-12-26 17:59:52 -0800

    inital smartsheet queries for followers  CRUD

--- controllers/usersSheet.js
+++ controllers/usersSheet.js
@@ -15,7 +15,27 @@ usersSheet.get("/all", async (req, res, err) => {
   };
 
   const allUsers = await smartsheet.sheets.getSheet(options);
+  console.log("All Users", allUsers);
   res.status(200).send("Grabbing all Users!");
+});
+// View user
+usersSheet.get("/view/:id", async (req, res, err) => {
+  var options = {
+    sheetId: process.env.SMARTSHEET_USER_SHEET_ID,
+    queryParameters: {
+      query: `"${req.params.id}"`
+    }
+  };
+
+  const friendOptions = {
+    sheetId: process.env.SMARTSHEET_USER_SHEET_ID,
+    rowId: await smartsheet.search
+      .searchSheet(options)
+      .then(res => res.results[0].objectId)
+  };
+  const friendData = await smartsheet.sheets.getRow(friendOptions);
+  console.log('Friend Data'. friendData);
+  res.status(200).send("viewing friend profile");
 });
 // Follow user
 usersSheet.post("/followers/add", async (req, res, err) => {
@@ -24,6 +44,20 @@ usersSheet.post("/followers/add", async (req, res, err) => {
   get followers
   add new follower
   */
+ // Set options
+var options = {
+  id: process.env.SMARTSHEET_USER_SHEET_ID // Id of Sheet
+};
+
+// Saved some data manip for you
+const userToAdd = allUserssmartsheet.sheets.getSheet(options)
+    .then(function(sheetInfo) {
+        console.log(sheetInfo);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+
   res.status(200).send("Added new user");
 });
 // Remove a followed user
