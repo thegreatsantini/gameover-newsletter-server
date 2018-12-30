db3e1798ca1a8e14816f6aea2dbd172d085eaf98
thegreatsantini <lss13@my.fsu.edu>
2018-12-29 22:02:42 -0800

    sending data for all users

--- controllers/user.js
+++ controllers/user.js
@@ -23,22 +23,65 @@ user.get("/:id", async (req, res, err) => {
       .searchSheet(userSearchOptions)
       .then(res => res.results[0].objectId)
   };
-  const currentUserData = await smartsheet.sheets.getRow(options);
-  const keys = [
+
+  const gameKeys = ["title", "console", "available", "received", "genres"];
+  const userKeys = [
+    "rowId",
     "id",
     "email",
     "userName",
-    "password",
     "watchlist",
     "following"
   ];
-  const massagedUser = currentUserData.cells.reduce((acc, next, i) => {
-    if (i != 3) {
-      acc[keys[i]] = next.value;
-    }
-    return acc;
-  }, {});
-  res.status(200).send({ "current user data!": massagedUser });
+
+  const allGames = await smartsheet.sheets
+    .getSheet({ id: process.env.SMARTSHEET_GAME_SHEET_ID })
+    .then(res => {
+      return res.rows.reduce((parentObj, parentNext, index) => {
+        parentObj[parentNext.id] = parentNext.cells.reduce((acc, next, i) => {
+          acc[gameKeys[i]] = next.value || "";
+          return acc;
+        }, {});
+        return parentObj;
+      }, {});
+    })
+    .catch(err => console.log(err));
+  const allUsers = await smartsheet.sheets
+    .getSheet({ id: process.env.SMARTSHEET_USER_SHEET_ID })
+    .then(res => {
+      return res.rows.reduce((parentObj, parentNext, index) => {
+        parentObj[parentNext.id] = parentNext.cells.reduce((acc, next, i) => {
+          if (!acc.hasOwnProperty("rowId")) {
+            acc[userKeys[i]] = parentNext.id;
+          } else {
+            acc[userKeys[i]] = next.value || "";
+          }
+          return acc;
+        }, {});
+        return parentObj;
+      }, {});
+    })
+    .catch(err => console.log(err));
+  try {
+    const currentUserData = await smartsheet.sheets.getRow(options);
+    const mySpit = data => (data.length > 1 ? data.split(",") : [data]);
+    const { cells } = currentUserData;
+    const userObj = {
+      userId : cells[0].value,
+      email: cells[1].value,
+      userName: cells[2].value,
+      watchlist: mySpit(currentUserData.cells[4].displayValue.trim()).map(
+        val => allGames[val]
+      ),
+      followers: mySpit(currentUserData.cells[5].displayValue.trim()).map(
+        val => allUsers[val]
+      )
+    };    
+    res.status(200).send(userObj);
+  } catch (err) {
+    console.log("err", err);
+    res.status(500).json({ error: err.toString() });
+  }
 });
 
 // Add game to watchlist
@@ -73,6 +116,7 @@ user.put("/watchlist/add", async (req, res, err) => {
     await smartsheet.sheets.updateRow(addCellOptions);
   } catch (err) {
     console.log("err", err);
+    res.status(500).json({ error: err.toString() });
   }
   res.status(200).send("Added new game to watchlist");
 });
@@ -109,6 +153,7 @@ user.post("/watchlist/remove", async (req, res, err) => {
     await smartsheet.sheets.updateRow(addCellOptions);
   } catch (err) {
     console.log("err", err);
+    res.status(500).json({ error: err.toString() });
   }
   res.status(200).send("Removed game from watchlist");
 });
