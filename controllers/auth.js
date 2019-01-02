f25626410de4c1fa9d52dbbcff876cbd824f46b5
thegreatsantini <lss13@my.fsu.edu>
2019-01-01 23:18:09 -0800

    cleaned up code

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,37 +3,11 @@ const express = require("express");
 const auth = express.Router();
 const client = require("smartsheet");
 const uuidv1 = require("uuid/v1");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 const smartsheet = client.createClient({
   accessToken: process.env.SMARTSHEET_TOKEN,
   logLevel: "info"
-});
-
-auth.get("/server/:email/:password", (req, res, err) => {
-  console.log(req.params)
-  // const options = {
-  //   sheetId: process.env.SMARTSHEET_USER_SHEET_ID,
-  //   queryParameters: {
-  //    query: `"${req.params.email}"`
-  //   }
-  // };
-
-  // smartsheet.search.searchSheet(options).then( (results)=> {
-  //   if (results.totalCount > 0) {
-  //     const userInfo = {
-  //       sheetId: process.env.SMARTSHEET_USER_SHEET_ID,
-  //       rowId: results.results[0].objectId
-  //     };
-  //     smartsheet.sheets.getRow(userInfo).then( row =>  {
-  //       const userPassword = rows.cells[3].displayValue;
-  //       // if (  )
-  //     })
-  //   } else {
-  //     res.status(200).send('User already exists, please Signup and to create a profile :)')
-  //   } 
-  // }).catch( err => console.log('error when searching for a current user', err))
-  res.status(300).send({"mesage":"hello from auth!"});
 });
 
 auth.post("/server/login", (req, res, err) => {
@@ -57,27 +31,30 @@ auth.post("/server/login", (req, res, err) => {
           .getRow(userInfo)
           .then(async row => {
             const userPass = row.cells[3].displayValue;
-            const checkPass = await bcrypt.compareSync(req.body.password, row.cells[3].displayValue);
+            const checkPass = await bcrypt.compareSync(
+              req.body.password,
+              row.cells[3].displayValue
+            );
             const person = {
-              "userId" : row.cells[0].value,
-              'userName' : row.cells[2].value,
-              'watchList' : row.cells[4].value
-            }
+              userId: row.cells[0].value,
+              userName: row.cells[2].value,
+              watchList: row.cells[4].value
+            };
             if (checkPass) {
               res.status(200).send(person);
             } else if (!checkPass) {
-              res.status(200).send({ "message" : "incorrect email or password"});
+              res.status(200).send({ message: "incorrect email or password" });
             } else {
-              res.status(400).send({"meassage":"something else went wrong"});
+              res.status(400).send({ meassage: "something else went wrong" });
             }
           })
           .catch(err => console.log("line 34", err));
       } else {
-        res.status(200).send({"message":"User not recognized"});
+        res.status(200).send({ message: "User not recognized" });
       }
     })
     .catch(function(error) {
-      console.log('error in search', error);
+      console.log("error in search", error);
     });
 });
 
@@ -91,7 +68,7 @@ auth.post("/server/signup", (req, res, err) => {
 
   smartsheet.search
     .searchSheet(options)
-    .then(async (results) => {
+    .then(async results => {
       if (results.totalCount === 0) {
         const salt = await bcrypt.genSaltSync(10);
         // add row in smart sheet
@@ -116,7 +93,7 @@ auth.post("/server/signup", (req, res, err) => {
               },
               {
                 columnId: process.env.WATCHLIST_COLUMN_ID,
-                value: ''
+                value: ""
               }
             ]
           }
@@ -132,17 +109,17 @@ auth.post("/server/signup", (req, res, err) => {
           .addRows(options)
           .then(function(newRows) {
             const person = {
-              "userId": newRows.result[0].cells[0].value, 
-              "userName": newRows.result[0].cells[2].value,
-              'watchList': ''
-            }
+              userId: newRows.result[0].cells[0].value,
+              userName: newRows.result[0].cells[2].value,
+              watchList: ""
+            };
             res.status(200).send(person);
           })
           .catch(function(error) {
             res.status(400).send("Unable to add new user");
           });
       } else {
-        res.status(200).send({message : 'User already exists'})
+        res.status(200).send({ message: "User already exists" });
         // res.status(200).send(results);
       }
     })
